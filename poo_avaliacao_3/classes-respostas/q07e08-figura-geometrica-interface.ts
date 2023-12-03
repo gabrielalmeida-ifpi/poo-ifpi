fb89b0fe65a88e7d7d92f5517f1ff7939e110195
gabrielalmeida-ifpi <126924497+gabrielalmeida-ifpi@users.noreply.github.com>
2023-12-03 20:06:15 -0300

    Update q07e08-figura-geometrica-interface.ts

--- poo_avaliacao_3/classes-respostas/q07e08-figura-geometrica-interface.ts
+++ poo_avaliacao_3/classes-respostas/q07e08-figura-geometrica-interface.ts
@@ -2,6 +2,12 @@
  * 7. Refaça a questão 04 do exercício usando interfaces com os métodos propostos
  * em vez de herança. Crie também um script que instancie e teste diferentes formas
  * geométricas.
+ *
+ * 8. Crie uma interface chamada IComparavel com um método chamado comparar que
+ * receba uma forma geométrica como parâmetro e retorna um inteiro como
+ * resultado. Implemente em cada uma das classes do exemplo anterior a interface
+ *retornando -1, 0 e 1 caso a área da forma seja menor, igual ou maior que a
+ * passada via parâmetro.
  */
 
 import { Teste } from "./conta"

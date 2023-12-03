d228e051d43a01561d19816802344515eb163862
gabrielalmeida-ifpi <126924497+gabrielalmeida-ifpi@users.noreply.github.com>
2023-12-03 20:06:26 -0300

    Update q07e08-figura-geometrica-interface.ts

--- poo_avaliacao_3/classes-respostas/q07e08-figura-geometrica-interface.ts
+++ poo_avaliacao_3/classes-respostas/q07e08-figura-geometrica-interface.ts
@@ -6,7 +6,7 @@
  * 8. Crie uma interface chamada IComparavel com um método chamado comparar que
  * receba uma forma geométrica como parâmetro e retorna um inteiro como
  * resultado. Implemente em cada uma das classes do exemplo anterior a interface
- *retornando -1, 0 e 1 caso a área da forma seja menor, igual ou maior que a
+ * retornando -1, 0 e 1 caso a área da forma seja menor, igual ou maior que a
  * passada via parâmetro.
  */
 
